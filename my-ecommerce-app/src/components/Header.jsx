260a4c6082ac7ceef0305acc54f9a288ccc9a096
Andre Philippe De Guzman <andrepdeguzman@gmail.com>
2025-10-03 17:18:09 +0800

    Necessary changes

---
+++ my-ecommerce-app/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <div class="">
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                        <li>Cart</li>
+                    </ul>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Header
